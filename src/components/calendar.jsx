fe02c286fdc82126b8a315752c32fe43a02e2b73
ainur <muharlyamovy@gmail.com>
2020-02-17 21:00:28 +0300

    0.2

--- src/components/calendar.jsx
+++ src/components/calendar.jsx
@@ -1,64 +1,68 @@
 import React, {Component} from "react";
+import Square from "./Square/square";
 
 
 class Calendar extends Component {
-    render() {
+    createCalendar = (year, month) => {
+        let mon = month - 1;
+        let date = new Date(year, mon);
 
-        function createCalendar(year, month) {
-            let mon = month - 1;
-            let date = new Date(year, mon);
+        const list = [];
 
-            const list = [];
+        list.push(
+            <div className="board-row">
+                <Square value = "Пн"/>
+                <Square value = "Вт"/>
+                <Square value = "Ср"/>
+                <Square value = "Чт"/>
+                <Square value = "Пт"/>
+                <Square value = "Сб"/>
+                <Square value = "Вс"/>
+            </div>
+        );
 
-            list.push(
-                <div className="board-row">
-                    <div>Пн</div>
-                    <div>Вт</div>
-                    <div>Ср</div>
-                    <div>Чт</div>
-                    <div>Пт</div>
-                    <div>Сб</div>
-                    <div>Вс</div>
-                </div>
-            );
+        let temp = [];
 
-            let temp = [];
+        for (let i = 0; i < this.getDay(date); i++) {
+            temp.push(<Square value = "-"/>);
+        }
 
-            for (let i = 0; i < getDay(date); i++) {
+
+        while (date.getMonth() === mon) {
+            temp.push(<Square value = {date.getDate()}/>);
+            if (this.getDay(date) % 7 === 6) {
+                list.push(<div className="board-row">{temp}</div>);
+                temp = [];
+            }
+
+            date.setDate(date.getDate() + 1);
+        }
+
+
+        if (this.getDay(date) !== 0) {
+            for (let i = this.getDay(date); i < 7; i++) {
                 temp.push(<div>-</div>);
             }
+        }
+
+        return list;
+
+    }
+
+    getDay = (date) => {
+        let day = date.getDay();
+        if (day === 0)
+            day = 7;
+        return day - 1;
+    }
 
 
-            while (date.getMonth() == mon) {
-                temp.push(<div>date.getDate()<div>);
-                if (getDay(date) % 7 == 6) {
-                    list.push(<div className="board-row">temp</div>);
-                }
-                        date.setDate(date.getDate() + 1);
-            }
+
+    render() {
+        return (this.createCalendar(2020, 2));
+    }
+
+}
 
 
-             if (getDay(date) != 0) {
-                 for (let i = getDay(date); i < 7; i++) {
-
-                  }
-                }
-
-                        return list;
-
-                        }
-
-                        function getDay(date) {
-                            let day = date.getDay();
-                            if (day == 0)
-                            day = 7;
-                            return day - 1;
-                        }
-
-                        return ( createCalendar(2020, 2) );
-                        }
-                        }
-                        }
-
-
-                        export default Calendar;
+export default Calendar;
