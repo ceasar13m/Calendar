4d34a6fde8fff88154b02054d2a0c2fc06ff76c4
ainur <muharlyamovy@gmail.com>
2020-03-24 21:14:19 +0300

    Updates

---
+++ precache-manifest.86eeea1632b4c414db93ed9b65ad2ff6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36b7cf478f24be5b58a88d0671296025",
+    "url": "/Calendar/index.html"
+  },
+  {
+    "revision": "1b3a78dbb8e8214e37dc",
+    "url": "/Calendar/static/css/main.19a01828.chunk.css"
+  },
+  {
+    "revision": "1966a6f324d1456d9e68",
+    "url": "/Calendar/static/js/2.fa910c4e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Calendar/static/js/2.fa910c4e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1b3a78dbb8e8214e37dc",
+    "url": "/Calendar/static/js/main.dca62676.chunk.js"
+  },
+  {
+    "revision": "1a225e043d282c08b503",
+    "url": "/Calendar/static/js/runtime-main.7a2bde9b.js"
+  }
+]);
